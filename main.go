5efbd50890557ad306d93f4c56dca3545e21d42a
liuzhanliang <zhanliangliu@qq.com>
2019-02-06 17:49:55 +0800

    added export func get_last_error

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ var (
 	mutex    sync.RWMutex
 	grammars []*fmr.Grammar
 	g        *fmr.Grammar
+	info     string
 )
 
 //export init_grammar
@@ -37,18 +38,23 @@ func init_grammar(s *C.char) int {
 	var err error
 	g, err = fmr.GrammarFromFile(f)
 	if err != nil {
-		fmt.Println(f, " ", err.Error())
-		panic(err)
+		info = fmt.Sprintln("load grammar file %s error: %+v", f, err.Error())
+		return -1
 	}
 	grammars = append(grammars, g)
 	index[f] = i
-	//fmt.Printf("grammar file loaded from %s\n", f)
 	return i
+}
+
+//export get_last_error
+func get_last_error() *C.char {
+	return C.CString(info)
 }
 
 //export extractx
 func extractx(i int, l, s *C.char) *C.char {
 	if i < 0 || i >= len(grammars) {
+		info = fmt.Sprintf("no grammar found with id %s", i)
 		return C.CString(`{"error":"no grammar"}`)
 	}
 	lg := grammars[i]
@@ -56,12 +62,14 @@ func extractx(i int, l, s *C.char) *C.char {
 	start := C.GoString(s)
 	trees, err := lg.ExtractMaxAll(line, start)
 	if err != nil {
+		info = fmt.Sprintf("ExtractMaxAll error: %+v", err)
 		return C.CString(`{"error":` + strconv.Quote(err.Error()) + `}`)
 	}
 	var ret []string
 	for _, tree := range trees {
 		sem, err := tree.Semantic()
 		if err != nil {
+			info = fmt.Sprintf("Semantic error: %+v", err)
 			return C.CString(`{"error":` + strconv.Quote(err.Error()) + `}`)
 		} else {
 			ret = append(ret, sem)
